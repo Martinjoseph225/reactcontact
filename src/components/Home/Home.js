2cccb34a975defe26b6de4e71b0325baf83b9331
Martin Joseph <martinjoseph225@gmail.com>
2021-12-04 23:24:32 +0530

    added new

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,10 +5,12 @@ const Home = () => {
   return (
     <div className="home">
       <h2 className="para1">
-        This is just a contact book to add and retrive data by react and
+        This App is just a contact book to add and retrive data by react and
         firebase
       </h2>
-      <h2 className="para1">You can build new contact and search the same</h2>
+      <h2 className="para1">
+        You can build a new contact and search for the same
+      </h2>
       <h2 className="para">* Add User to add new contact</h2>
       <h2 className="para">* Search User to search existing contact</h2>
       <h2 className="para">
@@ -21,6 +23,9 @@ const Home = () => {
         height="250px"
         alt="contact book"
       />
+      <h2 class="tech">
+        Tech stacks used: React.js, Firebase, Bootstrap, EmailJs
+      </h2>
     </div>
   );
 };
